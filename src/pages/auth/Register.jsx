6e449524ff178ab020b0676e139975dbb1ad4fc9
armmani <arm.maniii@gmail.com>
2025-06-19 13:59:17 +0700

    จัดการ sweetalert + reactHookForm install

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import FormInput from "../../components/form/FormInput";
+import { createAlert } from "../../utils/createAlert";
 
 function Register() {
+  // JS
+
+  const hdlSubmit = (e) => {
+    e.preventDefault()
+    createAlert("success", "REGISTERED")
+  }
   return (
     <div className="flex w-full h-full justify-end">
       {/* Card */}
       <div className="p-4 m-4 ">
 
         {/* Form */}
-        <form>
+        <form onSubmit={hdlSubmit}>
           <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 flex flex-col gap-4">
             <legend className="fieldset-legend">Register</legend>
             
