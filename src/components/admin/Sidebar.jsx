1b7082a3d216273915fab6c7b38ea99c22ed9138
armmani <arm.maniii@gmail.com>
2025-06-20 12:57:21 +0700

    ทำตาราง dashboard กับ user api

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,24 +1,34 @@
 import { CircleUserRound, LayoutDashboard } from "lucide-react";
 import { Link } from "react-router";
+import { sidebarLink } from "../../utils/links";
+import useAuthStore from "../../store/auth-store";
 
 function Sidebar() {
+  const user = useAuthStore((state) => state.user) // เรียกใช้ zustand จาก store แต่ใส่เฉพาะที่จะเอา อันนี้คือ user
   return (
     <div className="w-48 bg-neutral">
       {/* Profile */}
       <div className="py-12 flex flex-col justify-center items-center">
         <CircleUserRound size={48} />
-        <p>Welcome</p>
+        <p>Welcome {user && user.role}</p>
       </div>
 
       {/* NavLink  */}
-      <Link to={"/admin"} className="text-white">
-        <div className="flex gap-2 px-4 py-4">
-          <span>
-            <LayoutDashboard />
-          </span>
-          <p>DashBoard</p>
-        </div>
-      </Link>
+      {sidebarLink.map((item) => {
+        return (
+          <Link 
+          key={item.label}
+          to={item.link} 
+          className="hover:bg-gray-400">
+            <div className="flex gap-2 px-4 py-4">
+              <span>
+                {item.icon}
+              </span>
+              <p>{item.label}</p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
--- src/pages/admin/Manage.jsx
+++ src/pages/admin/Manage.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 function Manage() {
   return (
-    <div>Manage</div>
+    <div>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>ROLE</th>
+        <th>ACTION</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row  */}
+      <tr>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+        <td>Blue</td>
+        <td>Blue</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+    </div>
   )
 }
 
